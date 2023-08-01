567b2ba60b17190d3aba09ebdf8df483ce7f06b2
R3288 <turbo.emamhasani@gmail.com>
2023-08-01 23:57:21 +0330

    webhook recived succuesfuly

--- data-reciver.py
+++ data-reciver.py
@@ -1,22 +1,28 @@
-from flask import Flask, request, abort
+import json
+from flask import Flask, request,jsonify, abort
 
 app = Flask(__name__)
 
-SECRET = "AzKosKhom1842"  # Replace with the secret key set in WooCommerce
+@app.route("/webhook", methods=["POST"])
+def webhook_receiver():
+    """This function will be called when a WooCommerce webhook is received.
 
-@app.route('/webhook', methods=['POST'])
-def receive_webhook():
-    secret = request.headers.get('X-WC-Webhook-Signature')
-    if secret != SECRET:
-        abort(403)  # Unauthorized request
-    
-    data = request.get_json()
-    
-    # Process the received webhook data
-    # Replace the following print statement with your own logic
-    print(data)
-    
-    return 'Webhook received successfully', 200
+    Args:
+        request (flask.Request): The HTTP request object.
+    """
+    data = request.data
+    print(f"Received data: {data}")
+    # webhook_data = json.loads(request.data)
+    # print(f"Received webhook event: {webhook_data['type']}")
+    # print(f"Webhook data: {webhook_data}")
+    try:
+        json_data = json.loads(data)
+        with open ('client_id.json','w') as file:
+            json.dump(json_data,file)
+    except json.JSONDecodeError as e:
+        return abort(400, f'Invalid JSON: {e}')
+    json_data[]
+    return "Webhook received!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
